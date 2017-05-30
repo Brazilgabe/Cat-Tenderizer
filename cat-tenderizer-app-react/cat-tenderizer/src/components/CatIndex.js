327a92e7e55c6e8781f929ac1b04bd2681a69ff0
gabriel giestas <ggiestas@gabriels-MacBook-Pro.local>
2017-05-30 10:59:54 -0700

    working copy, add new cats cats updated etc...

---
+++ cat-tenderizer-app-react/cat-tenderizer/src/components/CatIndex.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
+import CatListing from './CatListing'
+import catStore from '../stores/CatStore'
+
+class CatIndex extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      cats: catStore.getCats()
+    }
+  }
+
+  updateCats(){
+    this.setState({
+      cats: catStore.getCats()
+    })
+  }
+
+  componentWillMount(){
+    catStore.on('change', this.updateCats.bind(this))
+  }
+
+  renderCats(){
+    let catRender = []
+    for(var i=0; i<this.state.cats.length; i++){
+      let catId = "cat-" + i
+      catRender.push(
+        <CatListing key={catId} cat={this.state.cats[i]}></CatListing>
+      )
+    }
+    return catRender
+  }
+
+  render(){
+    return(
+      <div>
+        <div className="App-header">
+          <h2>Cat List</h2>
+          <div className="pull-right">
+            <Link to="/add">Add</Link>
+          </div>
+        </div>
+        <div className="cat-list row">
+          {this.renderCats()}
+        </div>
+      </div>
+    )
+  }
+}
+export default CatIndex;
